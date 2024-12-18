5b43f1f1f23c7524f6bbc0835f90fb08258b5e53
m39hira <akira20020107@gmail.com>
2024-12-18 17:37:48 +0900

    exection.pyで実行可能に変更

--- model/exection.py
+++ model/exection.py
@@ -3,30 +3,7 @@ from datetime import datetime, timedelta
 import os
 import json
 import mysql.connector
-
-def get_ble_data():
-    connection = mysql.connector.connect(
-        host="mysql",
-        user="project-e",
-        password="project-e",
-        database="ble_db",
-        port=3306
-    )
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM ble_data ORDER BY timestamp DESC LIMIT 1")  # 最新のデータを1件取得
-            results = cursor.fetchall()
-            if results:
-                _, timestamp, other_data = results[0]
-                other_data = json.loads(other_data)  # JSON文字列をPythonのリストに変換
-                return {"timestamp": timestamp, "other_data": other_data}
-            return None
-    finally:
-        connection.close()
-
-# 本来はモデル
-def model_function(data):
-    return 2
+import congestion_model
 
 # バッファファイルを初期化（新しい日付での利用時）
 def initialize_buffer():
@@ -56,10 +33,12 @@ def update_buffer():
 
         # 新しいデータの生成
         timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M')
-        ble_data = get_ble_data()
-
         timestamp = datetime.now()
-        prediction = model_function(ble_data)
+
+        # CatBoostモデルのファイルパス
+        model_path = "./best_catb_model.cbm"
+        # リアルタイム推定を開始
+        prediction = congestion_model.real_time_estimation(model_path)
         new_data = {"timestamp": timestamp, "prediction": prediction}
 
         print(new_data)
